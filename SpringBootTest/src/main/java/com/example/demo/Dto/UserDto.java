a80cf4104933e96d6590706d3bba8805e7c42a90
rwd117 <69449157+rwd117@users.noreply.github.com>
2021-05-25 18:28:32 +0900

    Delete SpringBootTest directory

--- SpringBootTest/src/main/java/com/example/demo/Dto/UserDto.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.Dto;
-
-public class UserDto {
-
-
-}
--- SpringBootTest/src/main/java/com/example/demo/Repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post,Long> {
-}
